42b89c5d1c2610834ee23db35e69064a69a615cf
aa35037123 <wesleyshi0321@gmail.com>
2024-05-17 21:50:28 +0800

    update: echo_bot

--- echo_bot.py
+++ echo_bot.py
@@ -3,4 +3,14 @@ import telebot
 TOKEN = "6355462545:AAF7_X-9X3JQnfWp_kvpKQ3eqBmNwwsa8bc"
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
+
 @bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+    
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.infinity_polling()
