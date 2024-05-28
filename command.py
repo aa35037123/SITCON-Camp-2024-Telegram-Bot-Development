37e2c97d8a26cbfae6c190bdd13e49d408057133
aa35037123 <wesleyshi0321@gmail.com>
2024-05-28 16:37:19 +0800

    command.py + honeset.py

---
+++ command.py
@@ -0,0 +1,8 @@
+import telebot
+
+TOKEN = "6355462545:AAF7_X-9X3JQnfWp_kvpKQ3eqBmNwwsa8bc"
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, f"Howdy, how are you doing?")
