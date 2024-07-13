4ebf9f0028d2d09a4470cd6366639417b1b3378a
aa35037123 <wesleyshi0321@gmail.com>
2024-07-13 12:07:37 +0800

    update command bot

--- command.py
+++ command.py
@@ -6,3 +6,4 @@ bot = telebot.TeleBot(TOKEN, parse_mode=None)
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, f"Howdy, how are you doing?")
+bot.infinity_polling()
